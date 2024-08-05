fd7255371a289aefac37d07ea0c79c43d914f408
Robert <robert@locastic.com>
2024-08-05 15:19:44 +0200

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import React from 'react';
+import * as Location from 'expo-location';
 import { useFonts } from 'expo-font';
+import * as TaskManager from 'expo-task-manager';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
@@ -20,6 +27,17 @@ export const unstable_settings = {
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+export const LOCATION_TASK_NAME = 'background-location-task';
+
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    return;
+  }
+  if (data) {
+    console.log('ROUTER data:', data);
+  }
+});
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
@@ -47,6 +65,31 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
+
+  async function requestPermissions() {
+    const requestForeground = Location.requestForegroundPermissionsAsync;
+    const requestBackground = Location.requestBackgroundPermissionsAsync;
+
+    const foregroundRequest = await requestForeground();
+    if (foregroundRequest.granted) {
+      const backgroundRequest = await requestBackground();
+      if (backgroundRequest.granted) {
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+          accuracy: Location.Accuracy.Highest,
+          activityType: Location.LocationActivityType.Fitness,
+          foregroundService: {
+            notificationTitle: 'Location',
+            notificationBody: 'Tracking your location',
+          },
+        });
+      }
+    }
+    return false;
+  }
+
+  React.useEffect(() => {
+    requestPermissions();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
